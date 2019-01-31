46aaa9503c786d425ef119f7d96faa72a0bb080d
Endre <tamas.nagy.endre@gmail.com>
2019-01-31 23:12:46 +0100

    after getting the length of the video

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 //import electron as dependency
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
 //main process with app object
-// const {property1, property2,..} = object;
-const {app, BrowserWindow}=electron;
+// const {property1, property2,..} = libary;
+const {app, BrowserWindow, ipcMain}=electron;
 
 app.on('ready', ()=>{
     // when the browser window object is created, basically a window is created. 
@@ -11,9 +12,13 @@ app.on('ready', ()=>{
 
     //loads a valid URL into the specified window object
     mainWindow.loadFile('index.html') //from official guide
-    //mainWindow.loadURL('file://${__dirname}\\index.html'); //look into current working directory
-
-
+    //mainWindow.loadURL('file://${__dirname}\\index.html'); //look into current working director
     //console.log('App is now ready');
+});
+
+ipcMain.on('video:submit', (event, path)=>{
+    ffmpeg.ffprobe(path, (err, metadata)=>{
+        console.log('Video duration is : ', metadata.format.duration);
+    });
 });
 
